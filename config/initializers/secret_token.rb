e9ca240c83ce66f49c42394d64a5daf7ded18358
WojtekM <wojciech.mardyla@gmail.com>
2012-07-14 16:17:06 +0200

    Init dla thapp.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thapp::Application.config.secret_token = '4519bd092600ab80bdabb1613e24db9fa45546720624956ba414943af98e9da48c2d6480b2df33306c2d5c35abc4689429de8128905749c1e79aebfad308eec8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Thapp::Application.config.session_store :cookie_store, key: '_thapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Thapp::Application.config.session_store :active_record_store
